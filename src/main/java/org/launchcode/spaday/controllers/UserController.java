b6e91df1fdf825bdd51656167b8c0e62895fdb66
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-05-04 19:00:42 -0500

    studio 5/4 spa day starter

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,28 @@
+package org.launchcode.spaday.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+    /*
+    1.create handler method displayAddUserForm() to render
+    correspond to path /user/add
+    return path to add.html template
+        */
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "redirect:";
+    }
+
+    //4. add handler method from 14.6.4
+    //uses model binding to create a new user object, user, and pass it into handler method
+
+    //5. check that verify parameter matches password within the user object
+    //if so, render user/index.html with a message that welcomes by user
+    //if passwords dont match, ender again
+
+
+}
