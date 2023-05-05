8902891c1ea646e5318ecc1b6c148a0544151837
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-05-05 00:11:35 -0500

    studio 5/4 spa day

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,28 +1,42 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("user")
 public class UserController {
-    /*
-    1.create handler method displayAddUserForm() to render
-    correspond to path /user/add
-    return path to add.html template
-        */
-    @GetMapping("add")
+
+    @GetMapping
+    public String user() {
+        return "user/index.html";
+    }
+
+    @GetMapping("/add")
     public String displayAddUserForm() {
-        return "redirect:";
+        return "user/add";
     }
 
     //4. add handler method from 14.6.4
     //uses model binding to create a new user object, user, and pass it into handler method
 
-    //5. check that verify parameter matches password within the user object
-    //if so, render user/index.html with a message that welcomes by user
-    //if passwords dont match, ender again
+    @PostMapping("/add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        // add form submission handling code here
+        if (user.getPassword().equals(verify)) {
+            return "user/add";
+        }
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("error", "Passwords do not match");
+        return "user/add";
 
+    }
 
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -6,6 +6,13 @@ public class User {
     private String email;
     private String password;
 
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
     public String getUsername() {
         return username;
     }
